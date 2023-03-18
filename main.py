bee90a0d1eccaca60966c97d31780637614c844c
nate-hauser <128199246+nate-hauser@users.noreply.github.com>
2023-03-17 23:28:20 -0500

    capture input files

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 import pandas as pd
 import table_editor as te
 
+test = True
+#test = None
 
 headers = ['Sow ID', 'Group', 'Date Breed', 'Breeder Initials', 'Date Farrowed', 'Liter']
 sow_ids = [1, 2, 3, 4]
@@ -12,9 +14,6 @@ breeder_initials = ['MK', 'MK', 'NN', 'NN']
 date_farrowed = ['6/19', '6/21', '6/21', '6/25']
 liter = [10, 8, 11, 7]
 
-test_data = str([sow_ids, groups, date_breed, breeder_initials, date_farrowed, liter])
-#print(test_data)
-
 data = {
     'Sow ID' : sow_ids,
     'Group' : groups,
@@ -23,30 +22,6 @@ data = {
     'Date Farrowed' : date_farrowed,
     'Liter Size' : liter
 }
-
-# dataframe = pd.DataFrame(data)
-# table_headers = dataframe.columns
-# table_headers = table_headers.values
-# table_values = dataframe.to_numpy()
-# table_headers = list(table_headers)
-
-
-# popup_layout = [[
-#             sg.Table(values=table_values, headings=table_headers, justification='center'),
-#             sg.Button('Done')
-#         ]]
-
-# t_window = sg.Window('Table', popup_layout)
-# # gui_table = values['Table']
-# # window['Table']
-# while True:
-#     t_event, t_values = t_window.read()
-#     if t_event == "Done" or t_event == sg.WIN_CLOSED:
-#         # element = window['End_Date']
-#         # print(element.Get())
-#         break
-
-# exit()
 
 #*****************layout**********************
 title = "Bread/Farrow Data Uploader"
@@ -73,22 +48,6 @@ input_layout = [
     sg.FileBrowse()
 ]
 
-# #Image Folder Directory
-# image_box = sg.Listbox(
-#             values=[], enable_events=True, size=(40, 20), key="Image_List"
-#         )
-
-# image_list_layout = [
-#     [
-#         sg.Text('Image directory'),
-#         sg.In(size=(20,1),enable_events=True,key='Image_Folder'),
-#         sg.FolderBrowse()
-#     ],
-#     [
-#         image_box
-#     ]  
-# ]
-
 output_box = sg.Multiline(key='-Output-', size=(50, 10), reroute_stdout=False)
 
 output_layout = [
@@ -99,15 +58,6 @@ output_layout = [
         output_box
     ]
 ]
-
-
-# column_layout = [
-#     [
-#         sg.Column(image_list_layout),
-#         sg.VSeparator(),
-#         sg.Column(output_layout)
-#     ]
-# ]
 
 close_layout = [
     sg.Button('Run'),
@@ -123,42 +73,38 @@ layout = [
 ]
 
 window = sg.Window(title, layout)
+breed_file = None
+farrow_file = None
 
 while True:
     event, values = window.read()
     if event == "Close" or event == sg.WIN_CLOSED:
-        # element = window['End_Date']
-        # print(element.Get())
         break
-    # Folder name was filled in, make a list of files in the folder
-    # if event == "Image_Folder":
-    #     folder = values["Image_Folder"]
-    #     try:
-    #         # Get list of files in folder
-    #         file_list = os.listdir(folder)
-    #     except:
-    #         file_list = []
+    elif event == "Run":
+        if breed_file == None or farrow_file == None:
+            if not test:
+                print('Missing Files')
+                continue
+        #FUNCTION CALL: breed_df, farrow_df, breed_uncert, farrow_uncert = funct(breed_file, farrow_file)
+            #The uncert would be a list of the sort for each cell in question: [[row1, col1], [row2, col2]]
 
-    #     fnames = [
-    #         f
-    #         for f in file_list
-    #         if os.path.isfile(os.path.join(folder, f))
-    #         and f.lower().endswith((".txt", ".gif"))
-    #     ]
-    #     window["Image_List"].update(fnames)
-    if event == "Run":
-        #out_element = window['Output']
-        #out_element.print('Testing')
-        #out_element.reroute_stdout_to_here()
-        #print('Testing')
-        
-        #pandas_dataframe = data
+        #Have user Validate
         dataframe = pd.DataFrame(data)
         t_data = te.table_editor(dataframe)
         print(t_data)
 
-    if event == 'Print':
+        #FUNCTION CALL: data_good = check_data(df)
+
+        #if data good, proceed
+
+        #FUNCTION CALL: update_spreadsheet(new_breed_df, new_farrow_df)
+
+    elif event == 'Print':
         print('Printing')
-    #if event == 'Select Date':
-        #print(window.find_element('-End_Date-'))
+    elif event == '-Breeding File-':
+        breed_file = window['-Breeding File-'].get()
+        #print(breed_file)
+    elif event == '-Farrow File-':
+        farrow_file = window['-Farrow File-'].get()
+
 
